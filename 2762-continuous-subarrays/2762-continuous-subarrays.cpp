1b679a095d8d6632b5793c2e8e11feb8a78507e8
Abdullah Al Ghalib <77296862+al-ghalib@users.noreply.github.com>
2024-12-15 03:13:50 +0600

    Time: 91 ms (82.15%), Space: 112.8 MB (89.16%) - LeetHub

---
+++ 2762-continuous-subarrays/2762-continuous-subarrays.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    long long continuousSubarrays(vector<int>& nums) {
+        
+        int n = nums.size();
+        
+        map<int, int> mp;
+        
+        int i = 0, j = 0;
+        
+        long long cnt = 0;
+        
+        while(j < n)
+        {
+            mp[nums[j]]++;
+            
+            while(abs(mp.rbegin()->first - mp.begin()->first) > 2)
+            {
+                mp[nums[i]]--;
+                
+                if(mp[nums[i]] == 0) mp.erase(nums[i]);
+                
+                i++;
+            }
+            
+            cnt += j - i + 1;
+            
+            j++;
+        }
+        
+        return cnt;
+    }
+};
